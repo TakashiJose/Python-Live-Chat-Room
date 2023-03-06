4c15837ab9d23bab3b7a84f6e1db0abf1a4229af
Jose Sanrindo <49176895+TakashiJose@users.noreply.github.com>
2023-03-05 16:35:14 -0800

    Room_code

--- main.py
+++ main.py
@@ -4,11 +4,41 @@ import random
 from string import ascii_uppercase
 
 app = Flask(__name__)
-app.config["KEY"] = "hjjj"
+app.config["SECRET_KEY"] = "hjjj"
 socketio = SocketIO(app)
+
+rooms={}
+
+def generate_unique_code(Length):
+    while True:
+        code=""
+        for _ in range(Length):
+            code+= random.choice(ascii_uppercase)
+        if code not in rooms:
+            break
+    return code
 
 @app.route("/", methods=["POST", "GET"])
 def home():
+    if request.method=="POST":
+        name = request.form.get("name")
+        code = request.form.get("code")
+        join = request.form.get("join", False)
+        create=request.form.get("create", False)
+
+        if not name:
+            return render_template("home.html", error="Please enter a name.")
+        
+        if join != False and not code:
+            return render_template("home.html", error="Please enter a room code.")
+        
+        room = code
+        if create != False:
+            room = generate_unique_code(4)
+            rooms[room] = {"members": 0, "messages":[]}
+        elif code not in rooms: 
+            return render_template("home.html", error="Room doesn't exist.")
+        
     return render_template("home.html")
 
 if __name__=="__main__":
